8a5255711614e2f58321d6e454cbbc45070b2f20
Gabriel Ifoga <gabrielifoga@yahoo.com>
2023-08-18 18:04:30 -0400

    fixes

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-app.use(express.json());
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 
-
 const postRoutes = require('./routes/posts');
-
-const PORT = process.env.PORT || 3000;
-
 const userRoutes = require('./routes/users');
 
-
-app.use('/api/users', userRoutes);
-app.use('/api/posts', postRoutes);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
+app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// Regular Express Server
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Regular Express server is running on port ${PORT}`);
 });
+
+// Serverless Configuration
+const serverless = require('serverless-http');
+const serverlessApp = express();
+
+serverlessApp.use(express.json());
+serverlessApp.use('/api/users', userRoutes);
+serverlessApp.use('/api/posts', postRoutes);
+serverlessApp.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+module.exports.handler = serverless(serverlessApp);
